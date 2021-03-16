3356def3e65af7ac3d81b2b5d0f6ff87f77182be
Peter Broom <me@peterbroom.co.uk>
2021-03-16 11:44:20 +0000

    make cloudinary folder name dynamic

--- utilities/shortcodes/shortcodes.js
+++ utilities/shortcodes/shortcodes.js
@@ -1,23 +1,30 @@
+const path = require('path');
 const CLOUDNAME = "peterbroom"
-const FOLDER = "peterbroom_portfolio"
 const BASE_URL = `https://res.cloudinary.com/${CLOUDNAME}/image/upload/`;
 const FALLBACK_WIDTHS = [ 300, 600, 680, 1360 ];
 const FALLBACK_WIDTH = 680;
 
 function getSrcset(file, widths) {
-  const fileName = file.split(['/','\\']).pop();
   const widthSet = widths ? widths : FALLBACK_WIDTHS
   return widthSet.map(width => {
-    return `${getSrc(fileName, width)} ${width}w`;
+    return `${getSrc(file, width)} ${width}w`;
   }).join(", ")
 }
 
 function getSrc(file, width) {
-  const fileName = file.split(['/','\\']).pop();
-  return `${BASE_URL}q_auto,f_auto,w_${width ? width : FALLBACK_WIDTH}/${FOLDER}/${fileName}`
+  const folderName = file.split(path.sep)[7]; // split file path to get folder at position 7
+  const fileName = path.parse(file).base;
+
+  return `${BASE_URL}q_auto,f_auto,w_${width ? width : FALLBACK_WIDTH}/${folderName}/${fileName}`
+}
+
+function getImageWidth(file, width) {
+  console.log('width', width);
+  return `${width}`;
 }
 
 module.exports = {
   srcset: (file, widths) => getSrcset(file, widths),
   src: (file, width) => getSrc(file, width),
+  imageWidth: (file, width) => getImageWidth(file, width)
 }
